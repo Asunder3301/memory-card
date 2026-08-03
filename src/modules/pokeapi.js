23e5c2743591e16c4dbba379bf26bc8172951145
asunder <trashemail3301@gmail.com>
2026-08-03 12:38:24 -0400

    feat(PokeAPI): return object containg pokemon name/url

---
+++ src/modules/pokeapi.js
@@ -0,0 +1,22 @@
+export class PokeAPI {
+  static async #fetchData(searchTerm) {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+      );
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log("Error fetching data: ", error);
+    }
+  }
+
+  static async getData(searchTerm) {
+    const pokeObj = await this.#fetchData(searchTerm);
+    const name = pokeObj["forms"][0]["name"];
+    const image = pokeObj["sprites"]["front_default"];
+
+    return { name, image };
+  }
+}
