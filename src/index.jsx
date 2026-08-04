d3f549e497c05a3b108ce0e4c77b1f26e6e3c387
asunder <trashemail3301@gmail.com>
2026-08-04 14:13:02 -0400

    feat(ui): initalize game

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./components/App.jsx";
 import "./styles.css";
 import "./modern-normalize.css";
-
-function App() {
-  return <h1>Webpack React Template!</h1>;
-}
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find root element");
