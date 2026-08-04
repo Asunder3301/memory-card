363c2e0ffbc954c0aff4c4976d363e229de6b02a
asunder <trashemail3301@gmail.com>
2026-08-04 13:46:25 -0400

    fix(App): include index i in random selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ export function App() {
     //Loop through array backwards
     for (let i = array.length - 1; i > 0; i--) {
       //Pick random index from 0 to i
-      const j = Math.floor(Math.random() * (i - 1));
+      const j = Math.floor(Math.random() * (i + 1));
 
       //Swap elements
       [array[i], array[j]] = [array[j], array[i]];
